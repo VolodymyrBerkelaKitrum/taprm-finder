27f24d590e8b377b6fb1ad6571e10120faa4a966
Volodya Berkela <volodymyr.berkela@kitrum.com>
2020-10-28 10:37:22 +0200

    Welcome page customized by copy code from Shopify page

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+function initMap() {
+
+
+    var locations = [
+        ['Bondi Beach', -33.890542, 151.274856, 4],
+        ['Coogee Beach', -33.923036, 151.259052, 5],
+        ['Cronulla Beach', -34.028249, 151.157507, 3],
+        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+        ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: new google.maps.LatLng(-33.92, 151.25),
+    });
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+    }
+
+}
+
